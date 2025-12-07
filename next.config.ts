0661d489701e953c26d2d38948425ab93eeb5685
M Meah <1895121+mmeah@users.noreply.github.com>
2025-12-07 01:18:08 +0000

    How to build and push that code out to  github so the newly built static

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/dynamic-page-editor',
+  assetPrefix: '/dynamic-page-editor/',
   typescript: {
     ignoreBuildErrors: true,
   },
