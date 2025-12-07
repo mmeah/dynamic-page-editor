cf7e5c0bca606419d26b5923f1ab971d63104c91
M Meah <1895121+mmeah@users.noreply.github.com>
2025-12-07 02:09:17 +0000

    dynamic-page-editor/:1  GET https://mmeah.github.io/dynamic-page-editor/

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
   basePath: '/dynamic-page-editor',
+  assetPrefix: '/dynamic-page-editor/',
   typescript: {
     ignoreBuildErrors: true,
   },
