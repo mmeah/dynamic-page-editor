0ff812b9c0486cdfa241cba55c77bd2fbf5c9abe
mmeah <1895121+mmeah@users.noreply.github.com>
2025-12-13 10:39:59 -0600

    Allow release notes to be accessed when clicking on the version number

--- src/components/version-footer.tsx
+++ src/components/version-footer.tsx
@@ -1,8 +1,23 @@
-import { version } from '../../package.json';
+'use client';
+
+import { version, homepage } from '../../package.json';
 
 export function VersionFooter() {
+  const url = new URL(homepage);
+  const [user] = url.hostname.split('.');
+  const repo = url.pathname.substring(1);
+  const repoUrl = `https://github.com/${user}/${repo}`;
+  const releaseUrl = `${repoUrl}/releases/tag/v${version}`;
+
+  const handleClick = () => {
+    window.open(releaseUrl, '_blank');
+  };
+
   return (
-    <div className="fixed bottom-2 right-2 z-50 text-xs text-gray-400 pointer-events-none">
+    <div
+      className="fixed bottom-2 right-2 z-50 text-xs text-gray-400 cursor-pointer opacity-30 hover:opacity-80"
+      onDoubleClick={handleClick}
+    >
       v{version}
     </div>
   );
