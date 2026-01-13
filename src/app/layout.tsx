927aa53f5f48760d409540d9a95de5695fbbf5cc
mmeah <1895121+mmeah@users.noreply.github.com>
2026-01-13 13:02:45 -0600

    force refresh, dont allow cache (#23)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,10 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        {/* Prevent browser caching of the page */}
+        <meta httpEquiv="Cache-Control" content="no-store, no-cache, must-revalidate, max-age=0" />
+        <meta httpEquiv="Pragma" content="no-cache" />
+        <meta httpEquiv="Expires" content="0" />
       </head>
       <body className="font-body antialiased">
         <PageEditorProvider>
